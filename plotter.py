8ebc9fd978fbe3a05b9ba5c1e3353ad4ab0f6f5e
Heecheol Kim <kimhc6028@gmail.com>
2017-06-13 20:51:53 +0900

    plotter changed

--- plotter.py
+++ plotter.py
@@ -5,17 +5,24 @@ plt.style.use('ggplot')
 
 def subplot(subplot, bp, ft, title, test=True):
     plt.subplot(subplot)
-
-    x = np.arange(0, 40)
+    '''
+    """This averages training result for every [norm_range] results"""
+    if not test:
+        norm_range = 10
+        bp = [np.mean(bp[i:i+norm_range]) for i in range(len(bp)/norm_range)]
+        ft = [np.mean(ft[i:i+norm_range] )for i in range(len(ft)/norm_range)]
+    '''
+    if not (len(bp) == len(ft)):
+        print('Warning : Length of bp and ft are not equal. This may cause distortion in graph')
+    x = np.arange(0, len(bp))
     plt.plot(x, bp, color='r', label='backpropagation')
     plt.plot(x, ft, color='g', label='forward-thinking')
-    plt.legend(bbox_to_anchor=(1.0, 1.), loc=2, ncol=1, fontsize=15)
+    plt.legend(bbox_to_anchor=(1.0, 1.), loc=2, ncol=1, fontsize=20)
     axes = plt.gca()
-    axes.set_xlim([0, 40]) if not test else axes.set_xlim([0, 200])
     axes.set_ylim([0, 100])
-    plt.title(title, fontsize=20, y = 0.9)
-    plt.ylabel('Accuracy',fontsize=15)
-    plt.xlabel('Epochs',fontsize=15)
+    plt.title(title, fontsize=40, y = 1.05)
+    plt.ylabel('Accuracy', fontsize=30)
+    plt.xlabel('Epochs' if test else 'minibatches', fontsize=30)
     plt.grid(True)
 
 
@@ -24,37 +31,33 @@ try:
     bp_train, bp_test = pickle.load(f)
     f.close()
 except:
-    print('bp.pickle does not exist! Try \n
-    python backpropagation.py')
+    print('bp.pickle does not exist! Try \n python backpropagation.py')
 
 try:
     f = open('./result/bp_deep.pickle')
     dbp_train, dbp_test = pickle.load(f)
     f.close()
 except:
-    print('bp_deep.pickle does not exist! Try \n
-    python backpropagation.py --deep --epoch 200')
+    print('bp_deep.pickle does not exist! Try \n python backpropagation.py --deep --epoch 200')
 
 try:
     f = open('./result/ft.pickle')
     ft_train, ft_test = pickle.load(f)
     f.close()
 except:
-    print('ft.pickle does not exist! Try \n
-    python forward_thinking.py')
+    print('ft.pickle does not exist! Try \n python forward_thinking.py')
 
 try:
     f = open('./result/ft_deep.pickle')
     dft_train, dft_test = pickle.load(f)
     f.close()
 except:
-    print('ft_deep.pickle does not exist! Try \n
-    python forward_thinking.py --deep --epoch 200')
+    print('ft_deep.pickle does not exist! Try \n python forward_thinking.py --deep --epoch 200')
 
 
-subplot('221', bp_train, ft_train, 'Training Accuracy')
-subplot('222', bp_test, ft_test, 'Test Accuracy')
-subplot('223', dbp_train, dft_train, 'Training Accuracy (deep)')
-subplot('224', dbp_test, dft_test, 'Test Accuracy (deep)')
+subplot('221', bp_train, ft_train, 'Training Accuracy', False)
+subplot('222', bp_test, ft_test, 'Test Accuracy', True)
+subplot('223', dbp_train, dft_train, 'Training Accuracy (deep)', False)
+subplot('224', dbp_test, dft_test, 'Test Accuracy (deep)', True)
 
 plt.show()
